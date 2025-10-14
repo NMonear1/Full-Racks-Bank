e447339e63f7dbfb107ce3c91a2caa0e4d320a79
Christian Orozco <christianorozco@Christians-Laptop.local>
2025-10-14 01:32:06 -0700

    Added styling and placeholder text for account page

--- src/components/account/Account.jsx
+++ src/components/account/Account.jsx
@@ -1,22 +1,42 @@
-import { Link } from "react-router"
+import { Link } from "react-router";
 // import Checking from "./Checking"
 // import Savings from "./Savings"
 
 export default function Account() {
   return (
     <>
-      <div className="account"> 
+      <div className="account">
         <h1>Account summary</h1>
         <ul>
-          
-          <li>
-            <Link to="/checking"><h2>Checking</h2></Link>
-          </li>
-          <li>
-            <Link to="/savings"><h2>Savings</h2></Link>
-          </li>
-      </ul>
+          <Link to="/checking">
+            <li className="checking">
+              <h2>Checking</h2>
+              <div>
+                <p>Account number: 5555-55555</p>
+                <p>Available balance: $$$$$$</p>
+              </div>
+            </li>
+          </Link>
+          <Link to="/savings">
+            <li className="savings">
+              <h2>Savings</h2>
+              <div>
+                <p>Account number: 4444-44444</p>
+                <p>Available balance: $$$$$$</p>
+              </div>
+            </li>
+          </Link>
+          <Link to="/creditcard">
+            <li>
+              <h2>Credit Card</h2>
+              <div>
+                <p>Card number: 3333-33333</p>
+                <p>Available balance: $$$$$$</p>
+              </div>
+            </li>
+          </Link>
+        </ul>
       </div>
     </>
-  )
+  );
 }
